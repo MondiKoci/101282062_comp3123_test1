c80090a9ffed78d5c1d1374986c0f612997d676d
MondiKoci <mondi.koci@georgebrown.ca>
2021-10-05 17:51:29 -0400

    Question two finished.

---
+++ Question-2/callback.js
@@ -0,0 +1,20 @@
+
+function resolvedPromise(delay) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve({message:"delayed success!"}), delay);
+    });
+}
+
+
+function delayedException(delay){
+    return new Promise(function(resolve, reject) {
+        setTimeout(reject({error:"delayed exception!"}), delay);
+    });
+}
+
+resolvedPromise(500).then((message) => {
+    console.log(message);
+})
+delayedException(500).catch((error) =>{
+    console.log(error)
+})
