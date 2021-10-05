7c7f79199977b66b1f087842fee6df163cb5ae68
MondiKoci <mondi.koci@georgebrown.ca>
2021-10-05 17:08:59 -0400

    First Question finished

---
+++ Question-1/lower.js
@@ -0,0 +1,26 @@
+
+
+function lowerCaseWords(x){
+	let p2 = new Promise(function(resolve, reject){
+		let newArray = [];
+		let counter = 0;
+		for(let i = 0; i < x.length; i++){
+			if(typeof x[i] == "string"){
+				newArray[counter] = x[i].toLowerCase();
+				$counter++;
+			}
+		}
+		if(newArray.length > 0){
+			resolve(newArray)
+		}
+		else{
+			reject("FAILURE: No string elements found in the array")
+		}
+	});
+
+	return p2
+}
+const mixedArray = ['PIZZA', 10, true, 25, false, 'Wings'];
+lowerCaseWords(mixedArray).then(success => {
+	console.log(success)
+}, error => console.log(error))
